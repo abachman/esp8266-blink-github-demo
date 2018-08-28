424b51424def6f7f4e07761ad5db0882c0ee94f1
Adam Bachman <adam.bachman@gmail.com>
2018-08-28 14:20:50 -0400

    oh my god it's finally blinking

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,12 +1,16 @@
 
-
+#define LED 0
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+  delay(1000);
 }
